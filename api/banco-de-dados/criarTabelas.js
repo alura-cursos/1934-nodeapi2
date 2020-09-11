d3fec1cc8938efb698437a07e852dac5e6b7dae6
Matheus Hernandes <12698656+onhernandes@users.noreply.github.com>
2020-09-11 17:02:55 -0300

    video1.3

--- api/banco-de-dados/criarTabelas.js
+++ api/banco-de-dados/criarTabelas.js
@@ -1,6 +1,13 @@
-const ModeloTabela = require('../rotas/fornecedores/ModeloTabelaFornecedor')
+const modelos = [
+    require('../rotas/fornecedores/ModeloTabelaFornecedor'),
+    require('../rotas/fornecedores/produtos/ModeloTabelaProduto')
+]
 
-ModeloTabela
-    .sync()
-    .then(() => console.log('Tabela criada com sucesso'))
-    .catch(console.log)
+async function criarTabelas () {
+    for (let contador = 0; contador < modelos.length; contador++) {
+        const modelo = modelos[contador]
+        await modelo.sync()
+    }
+}
+
+criarTabelas()
---
+++ api/rotas/fornecedores/produtos/ModeloTabelaProduto.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize')
+const instancia = require('../../../banco-de-dados')
+
+const colunas = {
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    preco: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    estoque: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    },
+    fornecedor: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: require('../ModeloTabelaFornecedor'),
+            key: 'id'
+        }
+    }
+}
+
+const opcoes = {
+    freezeTableName: true,
+    tableName: 'produtos',
+    timestamps: true,
+    createdAt: 'dataCriacao',
+    updatedAt: 'dataAtualizacao',
+    version: 'versao'
+}
+
+module.exports = instancia.define('produto', colunas, opcoes)
