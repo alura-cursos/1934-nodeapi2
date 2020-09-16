6dd0eb4237695c85b7f014836531009b55b75c3f
Matheus Hernandes <12698656+onhernandes@users.noreply.github.com>
2020-09-16 16:01:55 -0300

    video4.2

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -23,6 +23,10 @@ roteador.post('/', async (requisicao, resposta, proximo) => {
         const serializador = new Serializador(
             resposta.getHeader('Content-Type')
         )
+        resposta.set('ETag', produto.versao)
+        const timestamp = (new Date(produto.dataAtualizacao)).getTime()
+        resposta.set('Last-Modified', timestamp)
+        resposta.set('Location', `/api/fornecedores/${produto.fornecedor}/produtos/${produto.id}`)
         resposta.status(201)
         resposta.send(
             serializador.serializar(produto)
@@ -57,6 +61,9 @@ roteador.get('/:id', async (requisicao, resposta, proximo) => {
             resposta.getHeader('Content-Type'),
             ['preco', 'estoque', 'fornecedor', 'dataCriacao', 'dataAtualizacao', 'versao']
         )
+        resposta.set('ETag', produto.versao)
+        const timestamp = (new Date(produto.dataAtualizacao)).getTime()
+        resposta.set('Last-Modified', timestamp)
         resposta.send(
             serializador.serializar(produto)
         )
@@ -78,6 +85,10 @@ roteador.put('/:id', async (requisicao, resposta, proximo) => {
     
         const produto = new Produto(dados)
         await produto.atualizar()
+        await produto.carregar()
+        resposta.set('ETag', produto.versao)
+        const timestamp = (new Date(produto.dataAtualizacao)).getTime()
+        resposta.set('Last-Modified', timestamp)
         resposta.status(204)
         resposta.end()
     } catch (erro) {
@@ -95,6 +106,10 @@ roteador.post('/:id/diminuir-estoque', async (requisicao, resposta, proximo) => 
         await produto.carregar()
         produto.estoque = produto.estoque - requisicao.body.quantidade
         await produto.diminuirEstoque()
+        await produto.carregar()
+        resposta.set('ETag', produto.versao)
+        const timestamp = (new Date(produto.dataAtualizacao)).getTime()
+        resposta.set('Last-Modified', timestamp)
         resposta.status(204)
         resposta.end()
     } catch (erro) {
